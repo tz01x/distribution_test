68632d2608445962050c0091260cddee14b918d9
tz01x <abudr963rahman@gmail.com>
2023-08-20 03:46:52 +0600

    based counting app

--- src/app.py
+++ src/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 count = 0
 
 @app.route('/')
 def index():
     global count
-    count += 1
+    return render_template('index.html', count=count)
+
+@app.route('/count-up')
+def countup():
+    global count
+    count+=1
+    return render_template('index.html', count=count)
+
+@app.route('/count-down')
+def countdown():
+    global count
+    count+=1
     return render_template('index.html', count=count)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port="5000",debug=True)
